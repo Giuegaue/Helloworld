63bb350a4bbd5d54ee8d01dbe44645c70668cb3c
Zachary_Houston <ziphouston@gmail.com>
2022-02-07 11:56:37 -0800

    Origin

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+public class Helloworld{
+    public static void main(String[] args){
+
+
+        String name = "My Name is Zachary Houston";
+        String age = "I am 23 years old";
+        String location = "My hometown is Solvang, CA";
+
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(location);
+    }
+}
